c0a7ad2db0d584505e205cfab7b551dd75f33cd6
Zain Karim <zain@zainkarim.com>
2025-05-31 00:39:28 -0500

    Added flashcards array, buttons to iterate, and flipping on tap

--- lexigo-ios/Views/FlashcardsView.swift
+++ lexigo-ios/Views/FlashcardsView.swift
@@ -7,29 +7,60 @@
 
 import SwiftUI
 
+let flashcards = [
+    Flashcard(word: "Juxtaposition", definition: "this is a definition"),
+    Flashcard(word: "Ameliorate", definition: "this is a definition"),
+    Flashcard(word: "Serendipitous", definition: "this is a definition"),
+    Flashcard(word: "Obfuscate", definition: "this is a definition")
+]
+
 struct FlashcardsView: View {
-    
+    @State var cardNumber = 0
     var body: some View {
         VStack{
             card
+            cardSelectorButtons
         }
         .padding()
     }
     
     var card: some View {
-        CardView(word: "Juxtaposition", definition: "This is a definition")
+        CardView(flashcard: flashcards[cardNumber])
+    }
+    
+    var cardSelectorButtons: some View {
+        HStack{
+            previousCard
+            Spacer()
+            nextCard
+        }
+    }
+    
+    func cardSelector(by offset: Int, label: String) -> some View{
+        Button(action: {
+            cardNumber += offset
+        }, label: {
+            Text(label)
+        })
+        .disabled(cardNumber + offset < 0 || cardNumber + offset >= flashcards.count)
+    }
+    
+    var previousCard: some View {
+        cardSelector(by: -1, label: "Previous")
+    }
+    
+    var nextCard: some View {
+        cardSelector(by: 1, label: "Next")
     }
 }
 
 struct CardView: View {
-    let word: String
-    let definition: String
-    
-    @State var isFaceUp = true
+    let flashcard: Flashcard
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
-            Rectangle()
+            RoundedRectangle(cornerRadius: 6)
             .fill(Color.white)
                 .shadow(radius: 5)
                 .frame(height: 250)
@@ -47,19 +78,23 @@ struct CardView: View {
     
     var faceUp: some View {
         VStack{
-            Text(word)
-                .font(.headline)
-            Text(definition)
-                .font(.subheadline)
+            Text(flashcard.definition)
         }
     }
     
     var faceDown: some View {
         VStack {
-            Text(word)
+            Text(flashcard.word)
                 .font(.title)
         }
     }
+}
+
+struct Flashcard {
+    let word: String
+    let definition: String
+    var tag: String? // optional
+    
 }
 
 #Preview {
